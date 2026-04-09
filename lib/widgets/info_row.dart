7960d80832981019a1d9a6f86bfebd5443dacd6c
nezukox01-hash <nezukox01@gmail.com>
2026-04-10 00:40:32 +0600

    Create info_row.dart

---
+++ lib/widgets/info_row.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class InfoRow extends StatelessWidget {
+  final String label;
+  final String value;
+
+  const InfoRow({
+    super.key,
+    required this.label,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+            label,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 15,
+            ),
+          ),
+        ),
+        Text(
+          value,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+      ],
+    );
+  }
+}
